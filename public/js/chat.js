9787bea74e87fcd15b502f81ea3ecbb5364684e5
Perseus[Perry] <34593362+Perseus-Perry@users.noreply.github.com>
2020-07-30 01:40:34 +0530

    room made mobile friendly

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,154 +1,138 @@
-var name = getCookie('name')
-var id = getCookie('id')
+var name = getCookie('name');
+var id = getCookie('id');
 var sk;
 var mutedUsers=  [];
 var toConnect = true;
+var startTime = 0 , forbiddenTags = ['img', 'div', 'span', 'script','button'];
+var pingsSent = 0;
+var sum = 0;
+var report;
 
+//REDIRECT TO LOGIN IF COOKIES NOT SET
 if(name === 'undefined') {
   //window.location.href = "https://jita.chat/auth";
   //toConnect=false;
 }
-
-var startTime;
-
-var forbiddenTags = ['img', 'div', 'span', 'script'];
-var pingsSent = 0;
-var sum = 0;
-
-$('.editable').each(function() {
-  this.contentEditable = true;
-});
+$('#m').focus();
+setInterval(sendPing, 1000); //time is in ms
+setInterval(resetBody,500); //temp fix for overflow
 
 if(toConnect){
-$(function() {
-  var socket = io({
-    'reconnection': true,
-    'reconnectionDelay': 1000,
-    'reconnectionAttempts': 25
-  });
+  $(function() {
 
-  socket.on('connect', function() {
-    socket.emit('newUserConnected', name, id);
+    var socket = io({'reconnection': true,'reconnectionDelay': 1000,'reconnectionAttempts': 25});
+
+    socket.on('connect', function() {
+      socket.emit('newUserConnected', name, id);
       sk = socket;
       $('.connectionStatus').text("Connected");
-  })
-
-  socket.on('disconnect',function(){
-    $('.connectionStatus').text("Disconnected. Attempting To Reconnect...");
-
-  })
-
-
-  socket.on('redirect',function(url){
-      window.location.href=url;
-  })
-
-
-  $('form').submit(function(e) {
-    e.preventDefault(); // prevents page reloading
-    //remove forbiddenInputs
-
-    removeTags();
-    //$('#m').html().replace(/\n/g, "<br>");
-    if($('#m').html() == '') {
-      return;
-    }
-    var scroll = false;
-    var toAdd = generateDiv(name,id, $('#m').html());
-    var chatbox = document.getElementById('chatBox');
-    if(chatbox.scrollTop >= (chatbox.scrollHeight - chatbox.offsetHeight)) {
-      scroll = true;
-    }
-    $('.chatBox').append(toAdd);
-    if(scroll) {
-      chatbox.scrollTop = chatbox.scrollHeight;
-    }
-    socket.emit('add chat message', $('#m').html(), name, id);
-    $('#m').html('');
-
-    return false;
-  });
-  socket.on('chat message', function(msg, name, id, socketID) {
-    if(socketID == socket.id) {
-      return;
-    }
-    if(mutedUsers.includes(unescape(name))){
-      return;
-    }
-    var scroll = false;
-    var toAdd = generateDiv(name, id, msg);
-    var chatbox = document.getElementById('chatBox');
-    if(chatbox.scrollTop >= (chatbox.scrollHeight - chatbox.offsetHeight)) {
-      scroll = true;
-    }
-    $('.chatBox').append(toAdd);
-    if(scroll) {
-      chatbox.scrollTop = chatbox.scrollHeight;
-    }
-
-
-  });
-
-
-  function sendPing() {
-    startTime = Date.now();
-    socket.emit('sendping');
-    pingsSent += 1;
-  }
-  setInterval(sendPing, 1000); //time is in ms
-
-  socket.on('sendpong', function() {
-    var time = Date.now();
-    var latency = (time - startTime) / 2;
-    sum += latency;
-    if(pingsSent % 5 == 0) {
-      var avg = sum / 5;
-      updatePing(avg);
-      sum = 0;
-    }
-  })
-
-
-  function updatePing(ping) {
-    $('.ping').html('Ping: ' + Math.trunc(ping) + " ms");
-  }
-
-
-  socket.on('onlineCount', function(onlineCount) {
-    $('.onlineCount').text(onlineCount);
-  });
-  socket.on('updateMemberList', function(userList) {
-    $('.memberList').empty();
-    userList.forEach((user) => {
-      var div = generateMemberDiv(user.name, user.id, user.socketID);
-      $('.memberList').append(div);
     });
+
+    socket.on('disconnect',function(){
+      $('.connectionStatus').text("Disconnected. Attempting To Reconnect...");
+    });
+
+    socket.on('redirect',function(url){
+        window.location.href=url;
+    });
+
+    socket.on('chat message', function(msg, name, id, socketID) {
+      if(socketID == socket.id) {
+        return;
+      }
+      if(mutedUsers.includes(unescape(name))){
+        return;
+      }
+      msg=$.trim(msg);
+      var scroll = false;
+      var toAdd = generateDiv(name, id, msg);
+      var chatbox = document.getElementById('chatBox');
+      if(chatbox.scrollTop >= (chatbox.scrollHeight - chatbox.offsetHeight)) {
+        scroll = true;
+      }
+      $('.chatBox').append(toAdd);
+      if(scroll) {
+        chatbox.scrollTop = chatbox.scrollHeight;
+      }
+    });
+
+    socket.on('sendpong', function() {
+      var time = Date.now();
+      var latency = (time - startTime) / 2;
+      sum += latency;
+      if(pingsSent % 5 == 0) {
+        var avg = sum / 5;
+        updatePing(avg);
+        sum = 0;
+      }
+    });
+
+    socket.on('onlineCount', function(onlineCount) {
+      $('.onlineCount').text(onlineCount);
+    });
+
+    socket.on('updateMemberList', function(userList) {
+      $('.memberList').empty();
+      userList.forEach((user) => {
+        var div = generateMemberDiv(user.name, user.id, user.socketID);
+        $('.memberList').append(div);
+      });
+    });
+
+    socket.on('userDisconnected', function(socketID) {
+      $('p:contains(' + socketID + ')').parents().eq(1).remove();
+    });
+
+    socket.on('ban',function(socketID){
+      console.log('ban');
+      if(socket.id===socketID){
+      window.location.href="https://jita.chat/banned";
+      }
+    });
+
   });
-  socket.on('userDisconnected', function(socketID) {
-    $('p:contains(' + socketID + ')').parents().eq(1).remove();
-  })
-  socket.on('ban',function(socketID){
-    console.log('ban');
-    if(socket.id===socketID){
-    window.location.href="https://jita.chat/banned";
-    }
-  });
-});
 }
 
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if(parts.length === 2) return parts.pop().split(';').shift();
-}
+
+//CONTROL FLOW MODIFIERS
+// ---------------------
+// ---------------------
+
+$('form').submit(function(e){
+
+  e.preventDefault();
+  removeTags();
+  if($('#m').html() == '')
+  {
+    return;
+  }
+  $('#m').html($.trim($('#m').html()));
+  var scroll = false;
+  var toAdd = generateDiv(name,id, $('#m').html());
+  var chatbox = document.getElementById('chatBox');
+
+  if(chatbox.scrollTop >= (chatbox.scrollHeight - chatbox.offsetHeight)) {
+    scroll = true;
+  }
+
+  $('.chatBox').append(toAdd);
+
+  if(scroll) {
+    chatbox.scrollTop = chatbox.scrollHeight;
+  }
+
+  sk.emit('add chat message', $('#m').html(), name, id);
+  $('#m').html('');
+  $('#m').focus();
+  return false;
+});
+
 
 $('#message').keypress(function(event) {
-
   if(event.keyCode == 13 && !event.shiftKey) {
     event.preventDefault();
+    $('#message').html($.trim($('#message').html()));
     $('#message').submit();
-    event.preventDefault();
-
   }
 });
 
@@ -160,41 +144,55 @@ document.getElementById('m').addEventListener("paste", function(e) {
   } else if(window.clipboardData) {
     text = window.clipboardData.getData('Text');
   }
-
-  // insert text manually
-  var newText = $('#m').html() + text;
+  removeTags();
+  var newText = $('#m').html() +" "+ text;
   $('#m').html(newText);
 });
 
+
+// ---------------------
+// ---------------------
+//CONTROL FLOW MODIFIERS
+
+
+// DIV GENERATORS
+// ---------------------
+// ---------------------
+
 function generateDiv(senderName, senderID, msg) {
-  src = '';
   src = 'https://images.evetech.net/characters/' + senderID + '/portrait';
   report = {nameToReport:unescape(senderName),IDtoReport:senderID,message:msg};
-  var div = '<div class="msg"><div><img class="potrait" src="' + src + '" /></div><div class="msgText"><p  style="white-space: pre-wrap"><a href="#" ondrag="onDrag(event)" class="name">' + unescape(senderName) + "</a><span class='report' onclick='reportUser(report)' title='Report Message'> !</span><span class='report' onclick='muteUser(report)' title='Mute Sender'> X</span> > <span class='normalText'>" + msg + '</span></p></div></div>';
+  var div = '<div class="msg"><div><img class="messagePotrait" src="' + src + '" /></div><div class="msgText"><p  style="white-space: pre-wrap"><a href="#" ondrag="onDrag(event)" class="name">' + unescape(senderName) + "</a><span class='report' onclick='reportUser(report)' title='Report Message'> !</span><span class='report' onclick='muteUser(report)' title='Mute Sender'> X</span>   >   <span class='normalText'>" + msg + '</span></p></div></div>';
   return div;
 }
 
 
 function generateMemberDiv(name, id, socketID) {
   src = 'https://images.evetech.net/characters/' + id + '/portrait';
-  var div = '<div class="member"><a href="#" ondrag="onDrag(event)"><div><img class="potrait" ondragstart="return false;" src="' + src + '" /></div><div class="memberText name"><p>' + unescape(name) + '</p></a><p style="visibility:hidden;position:absolute;">' + socketID + '</p></div></a><hr></div>';
+  var div = '<div class="member"><a href="#" ondrag="onDrag(event)"><div><img class="potrait" ondragstart="return false;" src="' + src + '" /></div><div class="memberText name"><a href="#" ondrag="onDrag(event)"><p>' + unescape(name) + '</p></a><p style="visibility:hidden;position:absolute;">' + socketID + '</p></div></div>';
   return div;
 }
 
-function onDrag(ev) {
-  ev.dataTransfer.setData("text", 'lol');
+
+// ---------------------
+// ---------------------
+// DIV GENERATORS
+
+
+function resetBody(){
+var body =   $('body');
+body.scrollTop = body.scrollHeight;
 }
 
-function removeTags() {
-  var message = $('#m');
-  forbiddenTags.forEach((tag) => {
-    message.find(tag).each(function(index) {
-      var text = $(this).text(); //get span content
-      $(this).replaceWith(text); //replace all span with just content
-    });
-  });
 
+function sendPing() {
+  startTime = Date.now();
+  sk.emit('sendping');
+  pingsSent += 1;
+}
 
+function updatePing(ping) {
+  $('.ping').html('Ping: ' + Math.trunc(ping) + " ms");
 }
 
 function drop(event) {
@@ -227,4 +225,21 @@ function muteUser(report){
     mutedUsers.push(report.nameToReport);
     alert('User Muted');
   }
+}
+
+function removeTags() {
+  var message = $('#m');
+  forbiddenTags.forEach((tag) => {
+    message.find(tag).each(function(index) {
+      var text = $(this).text();
+      $(this).replaceWith(text);
+    });
+  });
+}
+
+
+function getCookie(name) {
+  const value = "; ${document.cookie}";
+  const parts = value.split("; ${name}=");
+  if(parts.length === 2) return parts.pop().split(';').shift();
 }
